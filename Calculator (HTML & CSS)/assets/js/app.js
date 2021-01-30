bd1de09ac2a76fcf5a197d64059fb7202ec4c41d
Xander Cage <cagex491@gmail.com>
2021-01-31 00:34:39 +0300

    Finished Calculator

---
+++ Calculator (HTML & CSS)/assets/js/app.js
@@ -0,0 +1,47 @@
+// Selectors
+const squareRoot = document.querySelector('#squareRoot')
+const factorial = document.querySelector("#factorial")
+const clearEntry = document.querySelector("#clearEntry")
+const backspace = document.querySelector("#backspace")
+
+const equal = document.querySelector('#equals')
+const result = document.querySelector("#result")
+
+// Functions
+
+equal.addEventListener('click', () => {
+    var x = result.innerText.replace(/÷/g, '/')
+    var y = eval(x)
+    result.innerHTML = y
+})
+
+squareRoot.addEventListener('click', () => {
+    var x = result.innerText.replace(/÷/g, '/')
+    var y = Math.sqrt(x)
+    result.innerHTML = y
+})
+
+factorial.addEventListener('click', () => {
+    var x = result.innerText.replace(/÷/g, '/')
+
+    let answer = 1;
+    if(x === '') result.innerHTML = ''
+    else if (x === 0 || x === 1) {
+        result.innerHTML = answer
+    } else {
+        for (var i = x; i >= 1; i--) {
+            answer *= i
+        }
+        result.innerHTML = answer
+    }
+
+})
+
+clearEntry.addEventListener('click', () => {
+    result.innerHTML = ''
+})
+
+
+backspace.addEventListener('click', () => {
+    result.innerHTML = result.innerText.slice(0, -1)
+})
